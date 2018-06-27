5b40792b3d9b94e0f54d1e56667e52ac72da40bf
João B. Correa Jr <joao.correa@tecnoapp.com.br>
2018-06-27 14:01:02 -0300

    Commit inicial

---
+++ ClasseHorta.py
@@ -0,0 +1,11 @@
+'''
+Classe principal para gerenciamento da horta
+Metodos de irrigação, temperatura, humidade, etc
+'''
+
+class Horta(object):
+    def __init__(self):
+        print('Objeto criado')
+
+    def verificaTemperatura(self):
+        print('Temperatura do ambiente:.. ')
---
+++ hortaAutonoma.py
@@ -0,0 +1,8 @@
+'''
+Programa de execução principal
+'''
+from ClasseHorta import Horta
+
+print('teste')
+salsa = Horta()
+salsa.verificaTemperatura()
